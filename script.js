c54cde9fc3572eb3211ae383c7c7c0712fa05fde
BadMadTuna <147739282+BadMadTuna@users.noreply.github.com>
2023-10-31 19:34:22 +0100

    AI_update

--- script.js
+++ script.js
@@ -1,42 +1,15 @@
-window.addEventListener('load', function() {
-    const imageContainer = document.getElementById('image-container');
-    const imageFolder = 'images/'; // Folder with images in the same directory
+let slideIndex = 0;
+showSlides();
 
-    displayImages(imageFolder);
-
-    function displayImages(folderPath) {
-        const imgExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-
-        for (let i = 1; ; i++) {
-            let found = false;
-            for (const ext of imgExtensions) {
-                const imageSrc = `${folderPath}image${i}.${ext}`;
-                const imgElement = document.createElement('img');
-                imgElement.src = imageSrc;
-
-                imgElement.onerror = function() {
-                    // If the image doesn't exist, move to the next extension
-                    if (ext === imgExtensions[imgExtensions.length - 1]) {
-                        found = true;
-                    }
-                };
-
-                imgElement.onload = function() {
-                    // If the image exists, display it
-                    imageContainer.appendChild(imgElement);
-                    found = true;
-                };
-
-                if (found) {
-                    break;
-                }
-            }
-
-            if (!found) {
-                // No more images found, exit the loop
-                break;
-            }
-        }
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-});
-
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds (adjust as needed)
+}
